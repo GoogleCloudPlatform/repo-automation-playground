f87168ee36d0d7ebe87ed0fd45d3ef20b0574ebe
Ace Nassri <anassri@google.com>
2020-05-11 23:59:54 -0700

    Update region_tag_enforcer.sh

--- xunit-ci-enforcer/region_tag_enforcer.sh
+++ xunit-ci-enforcer/region_tag_enforcer.sh
@@ -41,30 +41,30 @@ function clean_describe {
 }
 
 function check_dir {
-	echo -e "Checking directory: \x1b[1m$(pwd)\x1b[0m"
-
 	SAMPLE_TAGS="$(grep -h "\[START" *.* | egrep -o '([a-z]|_)+' | sort -u)"
 
 	if [[ $(pwd) == *"nodejs"* ]]; then
-		echo -e 'Detected language: \x1b[92m\x1b[1mNode.js\x1b[0m'
+		LANG_MESSAGE='Detected language: \x1b[92m\x1b[1mNode.js\x1b[0m'
 		TEST_DESCRIBES=$(grep -h "describe(" test/*.*)
 		TEST_TAGS=$(clean_describe "$TEST_DESCRIBES")
 	elif [[ $(pwd) == *"ruby"* ]]; then
-		echo -e 'Detected language: \x1b[31m\x1b[1mRuby\x1b[0m'
+		LANG_MESSAGE='Detected language: \x1b[31m\x1b[1mRuby\x1b[0m'
 		TEST_DESCRIBES=$(grep -h "describe " spec/*.*)
 		TEST_TAGS=$(clean_describe "$TEST_DESCRIBES")
 	elif [[ $(pwd) == *"php"* ]]; then
-		echo -e 'Detected language: \x1b[36m\x1b[1mPHP\x1b[0m'
+		LANG_MESSAGE='Detected language: \x1b[36m\x1b[1mPHP\x1b[0m'
 		TEST_METHOD_NAMES=$(grep -h "function test" test/quick*.* | rev | cut -d' ' -f1 | rev | cut -c 5-)
 		TEST_METHOD_NAMES_SNAKE_CASE=$(snake_case "$TEST_METHOD_NAMES")
 		TEST_TAGS=$(clean_stop_words "$TEST_METHOD_NAMES_SNAKE_CASE")
 	elif [[ $(pwd) == *"python"* ]]; then
-		echo -e 'Detected language: \x1b[38;5;208m\x1b[1mPython\x1b[0m'
+		LANG_MESSAGE='Detected language: \x1b[38;5;208m\x1b[1mPython\x1b[0m'
 		TEST_CLASS_NAMES=$(grep -h "class " *_test.py | cut -d':' -f2 |  sed -E 's/^class (Test)*|\(\)$//g')
 		TEST_CLASS_NAMES_SNAKE_CASE=$(snake_case "$TEST_CLASS_NAMES")
 		TEST_TAGS=$(clean_stop_words "$TEST_CLASS_NAMES_SNAKE_CASE")
 	else
 		echo -e '\x1b[31m\x1b[1mERROR\x1b[0m No supported language detected!'
+		true
+		return # Do nothing
 	fi
 
 	DIFF="$(diff <(echo "$SAMPLE_TAGS") <(echo "$TEST_TAGS") | grep '_' | sort)"
@@ -72,20 +72,28 @@ function check_dir {
 	DIFF_LABELED=$(echo "$DIFF" | sed "s/</\\\nAdd tag to \\\x1b[91mTEST\\\x1b[0m:/g" | sed "s/>/\\\nAdd tag to \\\x1b[36mSNIPPET\\\x1b[0m:/g" | sort)
 
 	if [[ -n $DIFF_LABELED ]]; then
+		echo "--------------------------------------"
+		echo -e "Checking directory: \x1b[1m$(pwd)\x1b[0m"
+		echo -e "$LANG_MESSAGE"
 		echo -e $DIFF_LABELED
+		false
 	else
-		echo "No mismatched region tags detected."
+		# No mismatched region tags detected
+		true
 	fi
 }
 
+STATUS=0
 if [ "$#" -eq 0 ]; then
-	check_dir "$(pwd)"
+	check_dir "$(pwd)" || STATUS=1
 else
 	for DIR in "$@"
 	do
 		pushd "$DIR" > /dev/null
-	    check_dir "$DIR"
+	    check_dir "$DIR" || STATUS=1
 	    popd > /dev/null
-	    echo "--------------------------------------"
 	done
 fi
+
+# Fail if labels are missing
+exit $STATUS
