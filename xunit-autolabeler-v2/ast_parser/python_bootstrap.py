c9a63e34e1a0f3135749f515839a1689f87f58ea
ace-n <anassri@google.com>
2020-08-31 19:12:03 -0700

    Clean up python_bootstrap.py

--- xunit-autolabeler-v2/ast_parser/python_bootstrap.py
+++ xunit-autolabeler-v2/ast_parser/python_bootstrap.py
@@ -1,5 +1,17 @@
+import os
 import sys
 
 from python import invoker
 
-print(invoker.get_json_for_dir(sys.argv[1]))
+if len(sys.argv) != 2:
+    raise ValueError('Please specify exactly one [root] directory.')
+
+root_dir = sys.argv[1]
+output_file = os.path.join(root_dir, 'repo.json')
+
+json_out = invoker.get_json_for_dir(root_dir)
+with open(output_file, 'w') as f:
+    f.write(json_out + '\n')
+
+print(f'JSON written to: {output_file}')
+print('Do not move this file!')
