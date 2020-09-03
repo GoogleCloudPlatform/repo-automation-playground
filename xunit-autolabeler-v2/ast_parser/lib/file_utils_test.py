67b4c73bccd6c6adf3c326cdf1edc308d2b857f8
ace-n <anassri@google.com>
2020-09-03 16:45:06 -0700

    Add metadata region tag test

--- xunit-autolabeler-v2/ast_parser/lib/file_utils_test.py
+++ xunit-autolabeler-v2/ast_parser/lib/file_utils_test.py
@@ -81,3 +81,8 @@ class GetRegionTagsTest(unittest.TestCase):
         region_tags = file_utils.get_region_tags(TEST_DIR)
 
         assert 'region_tag' in region_tags
+
+    def test_finds_region_tags_in_metadata(self):
+        region_tags = file_utils.get_region_tags(TEST_DIR)
+
+        assert 'dockerfile_tag' in region_tags
