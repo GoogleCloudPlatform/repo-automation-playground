1b50c2f6d63e8c98ce8d3319588a22d59a764a36
ace-n <anassri@google.com>
2020-09-21 13:01:21 -0700

    Address comments

--- xunit-autolabeler-v2/ast_parser/python/test_data/parser/flask/flask_test.py
+++ xunit-autolabeler-v2/ast_parser/python/test_data/parser/flask/flask_test.py
@@ -13,15 +13,14 @@
 # limitations under the License.
 
 import pytest
+import main
 
 
 @pytest.fixture
 def app():
-    import main
     main.app.testing = True
     return main.app.test_client()
 
 
 def test_index(app):
-    r = app.get('/')
-    assert r.status_code == 200
+    assert app.get('/').status_code == 200
