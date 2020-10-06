0c4f191efd65b50c839023e68cff2b50338ae020
ace-n <anassri@google.com>
2020-10-05 23:49:02 -0700

    feat(AST parser): test data for polyglot parser

---
+++ xunit-autolabeler-v2/ast_parser/core/test_data/test_data/cli/additions/additions.py
@@ -0,0 +1,37 @@
+# Copyright 2020 Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# [START main_method]
+def main():
+    return 'main method'
+# [END main_method]
+
+
+# [START not_main]
+def not_main():
+    return 'not main'
+# [END not_main]
+
+
+# [START also_not_main]
+def also_not_main():
+    return 'also_not main'
+# [END also_not_main]
+
+
+# [START untested_method]
+def untested_method():
+    return 'untested!'
+# [END untested_method]
