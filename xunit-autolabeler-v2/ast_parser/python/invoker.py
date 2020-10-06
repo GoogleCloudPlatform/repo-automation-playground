19a328192d2d283022d673461f155052ef3a68a6
ace-n <anassri@google.com>
2020-10-05 21:04:45 -0700

    Add invoker files

---
+++ xunit-autolabeler-v2/ast_parser/python/invoker.py
@@ -0,0 +1,57 @@
+# Copyright 2020 Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import json
+import os
+
+from ast_parser.lib import file_utils
+from . import constants, source_parser, test_parser
+from typing import Any, List
+
+
+def _parse_source(source_path: str) -> List[Any]:
+    source_methods = source_parser.get_top_level_methods(source_path)
+    return [method for method in source_methods]
+
+
+def _parse_test(test_path: str, source_methods: List[Any]) -> None:
+    test_methods = test_parser.get_test_methods(test_path)
+    test_method_map = test_parser.get_test_key_to_snippet_map(test_methods)
+
+    test_parser.store_tests_on_methods(source_methods, test_method_map)
+
+
+def get_json_for_dir(root_dir: str) -> str:
+    python_files = file_utils.get_python_files(root_dir)
+
+    source_methods = []
+    source_files = [file for file in python_files
+                    if constants.TEST_FILE_MARKER not in file]
+
+    for file in source_files:
+        source_methods += _parse_source(file)
+
+    test_files = [file for file in python_files
+                  if constants.TEST_FILE_MARKER in file]
+
+    for file in test_files:
+        _parse_test(file, source_methods)
+
+    for method in source_methods:
+        new_source_path = os.path.relpath(
+            method.drift.source_path, root_dir)
+        method.drift._replace(source_path=new_source_path)
+
+    return json.dumps([method.drift._asdict() for method in source_methods])
---
+++ xunit-autolabeler-v2/ast_parser/python/invoker_test.py
@@ -0,0 +1,61 @@
+# Copyright 2020 Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import json
+import os
+
+
+from . import invoker
+
+
+TEST_DATA_PATH = os.path.join(
+    os.path.dirname(__file__),
+    'test_data/parser/edge_cases'
+)
+
+source_path = os.path.join(TEST_DATA_PATH, 'edge_cases.py')
+test_path = os.path.join(TEST_DATA_PATH, 'edge_cases_test.py')
+
+
+def test_recognizes_source_files():
+    methods = invoker._parse_source(source_path)
+
+    assert len(methods) == 1
+
+
+def test_recognizes_test_files():
+    methods = invoker._parse_source(source_path)
+
+    invoker._parse_test(test_path, methods)
+
+    assert len(methods) == 1
+    assert len(methods[0].drift.test_methods) == 1
+
+
+def test_get_json_for_dir():
+    repo_json = invoker.get_json_for_dir(TEST_DATA_PATH)
+
+    repo_obj = json.loads(repo_json)
+    assert len(repo_obj) == 1
+
+    first_repo_obj = repo_obj[0]
+
+    assert 'name' in first_repo_obj
+    assert 'class_name' in first_repo_obj
+    assert 'parser' in first_repo_obj
+    assert 'start_line' in first_repo_obj
+    assert 'end_line' in first_repo_obj
+    assert 'method_name' in first_repo_obj
+    assert 'test_methods' in first_repo_obj
