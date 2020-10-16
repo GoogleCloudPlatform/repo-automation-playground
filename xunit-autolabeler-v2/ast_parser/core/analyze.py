456ca913e4e4239b5a156ba1230cb3fadb8bd1b2
ace-n <anassri@google.com>
2020-10-16 15:16:00 -0700

    feat(AST parser): add analyze files

---
+++ xunit-autolabeler-v2/ast_parser/core/analyze.py
@@ -0,0 +1,127 @@
+# Copyright 2020 Google LLC. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+from os import path
+from typing import List, Tuple
+
+from . import polyglot_drift_data as pdd, polyglot_parser, yaml_utils
+
+
+def analyze_json(
+    repo_json: str,
+    root_dir: str
+) -> Tuple[List[str], List[str], List[str], List[pdd.PolyglotDriftData]]:
+    """Perform language-agnostic AST analysis on a directory
+
+    This function processes a given directory's language-specific
+    analysis (stored in a polyglot_snippet_data.json file) into a
+    list of automatically detected snippets. It then augments the
+    automatic detection results with useful manual data (specified
+    in .drift-data.yml files). Finally, it repackages all this data
+    into a tuple containing 4 useful lists of data as shown in the
+    'returns' section.
+
+    Arguments:
+        repo_json: A path to a polyglot_snippet_data.json file
+                   generated for the specified root_dir
+        root_dir: The root directory to perform AST analysis on
+
+    Returns:
+        A tuple containing the following:
+         - A list of tags found (via grep/text search)
+           within the given directory and its subdirectories
+         - A list of tags detected (by the AST parser)
+           within the given directory and its subdirectories
+         - A list of tags that the AST parser detected,
+           but chose to ignore (due to constants or user
+           specification in .drift-data.yml files)
+         - A list of snippet objects (as typed NamedTuples)
+           detected by the AST parser in the given directory
+           and its subdirectories
+    """
+    tuple_methods = []
+    with open(repo_json, 'r') as file:
+        json_content = '\n'.join(file.readlines())
+        json_methods = json.loads(json_content)
+
+        # Normalize source_path values
+        parent_path = path.dirname(repo_json)
+        for method in json_methods:
+            method['source_path'] = \
+                path.join(parent_path, method['source_path'])
+            method['test_methods'] = \
+                [tuple(test) for test in method['test_methods']]
+
+            tuple_methods.append(
+                pdd.PolyglotDriftData(**method))
+
+    source_filepaths = set([method.source_path for method in tuple_methods])
+
+    grep_tags = set()
+    ignored_tags = set()
+
+    for source_file in source_filepaths:
+        if not path.isfile(source_file):
+            raise ValueError(
+                f'Path {source_file} in file {repo_json} not found! '
+                'Did you move polyglot_snippet_data.json'
+                ' from its generated location?'
+            )
+
+        (region_tags, ignored_tag_names) = \
+            polyglot_parser.get_region_tag_regions(source_file)
+
+        grep_tags = grep_tags.union(set([t[0] for t in region_tags]))
+        ignored_tags = ignored_tags.union(set(ignored_tag_names))
+
+        for idx, method in enumerate(tuple_methods):
+            if method.source_path != source_file:
+                continue
+
+            new_method = \
+                polyglot_parser.add_region_tags_to_method(method, region_tags)
+            tuple_methods[idx] = new_method
+
+    source_methods = [method for method in tuple_methods if method.region_tags]
+
+    # Dedupe source methods
+    source_method_keys = set()
+    source_methods_deduped = []
+    for method in source_methods:
+        key = ','.join(method.region_tags)
+        if key not in source_method_keys:
+            source_methods_deduped.append(method)
+            source_method_keys.add(key)
+
+    source_methods = source_methods_deduped
+
+    polyglot_parser.add_children_drift_data(source_methods)
+    yaml_utils.add_yaml_data_to_source_methods(source_methods, root_dir)
+
+    source_tags = set()
+    for method in source_methods:
+        source_tags = source_tags.union(set(method.region_tags))
+
+    # Remove automatically ignored region tags from region tag lists
+    grep_tags = [tag for tag in grep_tags if tag not in ignored_tags]
+    source_tags = [tag for tag in source_tags if tag not in ignored_tags]
+
+    # Add manually ignored (via yaml) tags to ignored tags list
+    #   These should *not* overlap w/ source_tags, but we
+    #   check that in validate_yaml_syntax  - *not here!*
+    ignored_tags = ignored_tags.union(
+        yaml_utils.get_untested_region_tags(root_dir))
+
+    return (grep_tags, source_tags, list(ignored_tags), source_methods)
---
+++ xunit-autolabeler-v2/ast_parser/core/analyze_test.py
@@ -0,0 +1,134 @@
+# Copyright 2020 Google LLC. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import unittest
+
+from ast_parser.core import analyze
+
+import mock
+
+import pytest
+
+
+TEST_DIR = os.path.join(
+    os.path.dirname(__file__),
+    'test_data/parser'
+)
+
+
+class AnalyzeBasicTests(unittest.TestCase):
+    @pytest.fixture(autouse=True)
+    def _get_analyze_result(self):
+        basic_test_dir = os.path.join(TEST_DIR, 'edge_cases')
+        analyze_result = analyze.analyze_json(
+            os.path.join(basic_test_dir, 'polyglot_snippet_data.json'),
+            basic_test_dir
+        )
+
+        self.grep_tags = analyze_result[0]
+        self.source_tags = analyze_result[1]
+        self.ignored_tags = analyze_result[2]
+        self.source_methods = analyze_result[3]
+
+    def test_normalizes_source_paths(self):
+        assert self.source_methods != []
+
+        source_path = self.source_methods[0].source_path
+        assert source_path is not None
+        assert os.path.isabs(source_path)
+
+    def test_converts_test_methods_to_tuples(self):
+        assert self.source_methods != []
+
+        test_methods = self.source_methods[0].test_methods
+        assert test_methods != []
+
+        test = test_methods[0]
+        assert type(test) == tuple
+
+
+class AnalyzeMiscTests(unittest.TestCase):
+    def test_raises_error_on_missing_source_file(self):
+        with mock.patch('ast_parser.core.analyze.path') as path_mock:
+            path_mock.isfile.return_value = False
+
+            with self.assertRaisesRegex(ValueError, 'not found!'):
+                analyze.analyze_json(
+                    os.path.join(TEST_DIR, 'polyglot_snippet_data.json'),
+                    TEST_DIR
+                )
+
+    def test_dedupes_region_tags(self):
+        analyze_result = analyze.analyze_json(
+            os.path.join(TEST_DIR, 'polyglot_snippet_data.json'),
+            TEST_DIR
+        )
+
+        source_methods = analyze_result[3]
+        tag_sets = [method.region_tags for method in source_methods]
+
+        assert sum(tag_set == ['not_main'] for tag_set in tag_sets) == 1
+
+
+class AnalyzeMockCallTests(unittest.TestCase):
+    def test_adds_child_drift_data(self):
+        with mock.patch('ast_parser.core.analyze.polyglot_parser') \
+          as parser_mock:
+            parser_mock.get_region_tag_regions.return_value = ([], [])
+
+            analyze.analyze_json(
+                os.path.join(TEST_DIR, 'polyglot_snippet_data.json'),
+                TEST_DIR
+            )
+
+            source_path = os.path.abspath(
+                os.path.join(TEST_DIR, 'http/http_main.py'))
+            parser_mock.get_region_tag_regions.assert_any_call(source_path)
+
+    def test_adds_yaml_data(self):
+        with mock.patch('ast_parser.core.analyze.yaml_utils') \
+          as yaml_mock:
+            analyze.analyze_json(
+                os.path.join(TEST_DIR, 'polyglot_snippet_data.json'),
+                TEST_DIR
+            )
+
+            yaml_mock.add_yaml_data_to_source_methods.assert_called()
+
+
+class AnalyzeSmokeTests(unittest.TestCase):
+    @pytest.fixture(autouse=True)
+    def _get_analyze_result(self):
+        analyze_result = analyze.analyze_json(
+            os.path.join(TEST_DIR, 'polyglot_snippet_data.json'),
+            TEST_DIR
+        )
+
+        self.grep_tags = analyze_result[0]
+        self.source_tags = analyze_result[1]
+        self.ignored_tags = analyze_result[2]
+        self.source_methods = analyze_result[3]
+
+    def test_grep_tags_includes_undetected_tags(self):
+        assert 'main_method' not in self.source_tags
+        assert 'main_method' in self.grep_tags
+
+    def test_source_tags_only_includes_detected_tags(self):
+        assert 'not_main' in self.source_tags
+        assert 'main_method' not in self.source_tags
+
+    def test_omits_ignored_tags(self):
+        assert 'app' not in self.source_tags
+        assert 'app' in self.ignored_tags
