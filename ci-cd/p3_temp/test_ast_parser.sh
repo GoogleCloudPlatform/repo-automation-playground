84d1daa77c7d6713231831cb11f1e3f6ae24d340
ace-n <anassri@google.com>
2020-10-16 15:23:02 -0700

    Add directory to bootstrap gen

--- ci-cd/p3_temp/test_ast_parser.sh
+++ ci-cd/p3_temp/test_ast_parser.sh
@@ -34,6 +34,7 @@ pip install --user -r requirements-dev.txt
 ./python_bootstrap.py core/test_data/parser/http
 ./python_bootstrap.py core/test_data/parser/nested_tags
 ./python_bootstrap.py core/test_data/parser/webapp2
+./python_bootstrap.py core/test_data/parser/duplicate_region_tag
 ./python_bootstrap.py core/test_data/yaml
 ./python_bootstrap.py core/test_data/yaml/explicit_tests
 ./python_bootstrap.py core/test_data/yaml/invalid
